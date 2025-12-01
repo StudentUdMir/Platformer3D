73c6d0bf4140665083b12db0b6870a3b2b76a60a
StudentUdMir <stududmir@gmail.com>
2025-12-01 23:32:01 +0300

    Update Coin.cs

--- M3L4/Coin.cs
+++ M3L4/Coin.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Coin : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
-
-        CoinsCounter coins = other.GetComponent<CoinsCounter>();
-
+ 
+        Player player = other.GetComponent<Player>();
+ 
         //Количество монеток обновляется
-        coins.CollectCoins();
-
+        player.CollectCoins();
+ 
         //Монетка, которую собрали, уничтожается
         Destroy(gameObject);
     }
